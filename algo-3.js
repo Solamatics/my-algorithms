7886af8cea43520f8d26c358df99359178498de1
Olusola Afikode <afikodeolaitan@gmail.com>
2022-01-04 17:12:19 +0100

    passed algorithm on kata

--- algo-3.js
+++ algo-3.js
@@ -2,18 +2,27 @@
 //numbers given an array of minimum 4 positive integers. No floats or non-positive
 //integers will be passed.
 
-
 const sumOfTwoMinimumNumbers = (numbers) => {
-  const [firstNumber, secondNumber]= numbers.sort((a, b) => a - b);
+  const [firstNumber, secondNumber] = numbers.sort((a, b) => a - b);
   return firstNumber + secondNumber;
-}
+};
 
+console.log(sumOfTwoMinimumNumbers([12, 4, 96, 3]));
 
-console.log(sumOfTwoMinimumNumbers([12, 4, 96, 3]))
-
-//Given a string, replace every letter with its position in the 
+//Given a string, replace every letter with its position in the
 //alphabet. If anything in the text isn't a letter, ignore it and don't return it.
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-const alphabetPosition = () => {}
-  
+const alphabetPosition = (text) => {
+  return text
+    .toLowerCase()
+    .replace(/[^a-zA-Z]/g, "")
+    .split("")
+    .map((letter) => alphabet.indexOf(letter) + 1)
+    .join(' ');
+};
+
+let stringResult = alphabetPosition("The sunset sets at twelve o' clock.");
+
+console.log(stringResult);
